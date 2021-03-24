f0b40cbf0700c0a707f77072fb49c22b0ec05ff3
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2021-03-24 15:22:41 -0700

    added initial signup javascript

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,4 +1,22 @@
+var iti;
 var signup={};
+
+signup.validateFirstName = function(){
+    var firstNameValue =(document.getElementById('txtFirstName').value).trim();
+    if ((firstNameValue=='')|| (firstNameValue==null) || (firstNameValue=='undefined')){
+        return false;
+    }
+
+    return true;
+};
+signup.validateLastName = function(){
+    var lastNameValue =(document.getElementById('txtLastName').value).trim();
+    if ((lastNameValue=='')|| (lastNameValue==null) || (lastNameValue=='undefined')){
+        return false;
+    }
+
+    return true;
+};
 
 signup.validatePhone = function(){
     var phoneValue =(document.getElementById('phone').value).trim();
@@ -25,29 +43,78 @@ signup.validateEmail = function(){
     }
     return result;
 };
-signup.validateForm = function(){
-    if (signup.validateEmail()){
-        if (signup.validatePhone()){
-            if (signup.validateTermsOfPolicy()){
-                //SUBMIT FORM ----------------
-                app.showModal('success','FORM', "SUBMIT FORM")
+signup.validateForm = function(callback){
+    if (signup.validateFirstName()){
+        if (signup.validateLastName()){
+            if (signup.validateEmail()){
+                if (signup.validatePhone()){
+                    if (signup.validateTermsOfPolicy()){
+                        //SUBMIT FORM
+                        callback ("valid","valid");
+
+                    }else{
+                        callback('Policy Agreement', "Please agree to our terms of policy!")
+                    }
+                }else{
+                    callback('Phone Format', "Please Type-in a Valid Phone Number!");
+                }
             }else{
-                app.showModal('error','Policy Agreement', "Please agree to our terms of policy!")
+                callback('Email Format', "Please Type-in a Valid Email!")
             }
         }else{
-            app.showModal('error','Phone Format', "Please Type-in a Valid Phone Number!");
+            callback('Lastname Format', "Lastname cannot be empty!")
         }
     }else{
-        app.showModal('error','Email Format', "Please Type-in a Valid Email!")
+        callback('Firstname Format', "Firstname cannot be empty!")
     }
+
+};
+
+signup.handleRegistration = function(){
+    signup.validateForm(function(title, message){
+        if (title == 'valid'){
+            var firstName = document.getElementById('txtFirstName').value;
+            var lastName = document.getElementById('txtLastName').value;
+            var email = document.getElementById('txtEmail').value;
+            var phone = document.getElementById('phone').value;
+            var prefix = iti.getSelectedCountryData().dialCode;
+
+            var formData={
+                "firstName":firstName,
+                "lastName":lastName,
+                "email":email,
+                "phone":`${prefix}-${phone}`
+            };
+
+            var base = window.location.origin;
+            axios({
+                method: "post",
+                url: base+"/api/v1/user/initReg",
+                data:formData
+            })
+            .then(function(res){
+                app.showModal('success','Congrats!','User Registered successfully')
+
+            }).catch(function(err){
+                if (typeof(err.response)=="undefined"){
+                    app.showModal('error','Something went wrong!',"Request timeout!");
+                }else{
+                    app.showModal('error','Something went wrong!',err.response.data.data);
+                }
+            });
+
+        }else{
+            app.showModal('error',title, message)
+        }
+    });
 };
 
 signup.setSignupButton = function(){
     //formLinks
     var btnSignup = document.getElementById('signup');
     btnSignup.addEventListener('click',function(){
-        signup.validateForm();
-    });;
+        signup.handleRegistration();
+    });
 };
 
 app.init = function(){
@@ -58,7 +125,7 @@ app.init = function(){
     app.setTexFormLinks();
 
     var phoneField = document.getElementById("phone");
-    var iti = window.intlTelInput(phoneField, {
+    iti = window.intlTelInput(phoneField, {
         utilsScript: "js/utils.js?1613236686837"
     });
 };
