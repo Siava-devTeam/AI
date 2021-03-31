349011788f351b22d9c590276c6517747eabce79
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2021-03-31 13:14:18 -0700

    added route for signing in user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -6,6 +6,7 @@ const router = new express.Router();
 
 router.route("/getTokenUser").get(userCtl.getTokenUser);
 router.route("/initReg").post(userCtl.initialRegistration);
+router.route("/userSignin").post(userCtl.userSignin);
 router.route("/updateUserByToken").post(userCtl.updateUserByToken);
 router.route("/getAddressByToken").get(userCtl.getAddressByToken);
 router.route("/deleteUserToken").delete(userCtl.deleteUserToken);
