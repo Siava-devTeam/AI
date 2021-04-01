f8787e12421ae527484dc20b8b331269af401f1c
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2021-04-01 15:11:52 -0700

    added generalized send mail

--- api/controllers/helpers.js
+++ api/controllers/helpers.js
@@ -139,6 +139,127 @@ helpers.sendConfirmationMail = async function(link, userInfo){
     }
 
 };
+helpers.sendMailGeneral = async function(link, userInfo, type){
+    try{
+        var mailSubject= (type=="resetPass")
+        ?`Please Confirm you email ${userInfo.firstName} and Reset your Password`
+        :`Please Confirm you email ${userInfo.firstName}`;
+        var buttonText = (type=="resetPass")?"Reset Password":"Continue Registration";
+        //Mail Style
+        const pageWrapper = `
+        line-height: 1.5rem;
+        display: table;
+        margin: 0;
+        width: 100%;
+        height: 100vh;
+        background-color: #F3F4F6;
+        text-align: center;`;
+
+        const tblRow = `width: 100%;`;
+        
+        const frmWrapper = `
+        text-align: center;
+        background-color: #fff;
+        width:500px;
+        padding: 1.25rem;
+        margin: auto;`;
+        
+        const frmLogo = `
+        padding: 0.75rem 1rem;
+        width: 100%;`;
+        
+        const frmLogoImg =`width:400px;`;
+
+        const frmTitle = `
+        width: 100%;
+        color: #60A5FA;
+        font-size: 1.25rem;
+        line-height: 1.75rem;
+        padding: 0.75rem 1rem;`;
+        
+        const frmText = `
+        width: 100%;
+        color: #9CA3AF;
+        font-size: 1rem;
+        line-height: 1.25rem;
+        padding: 0.75rem 1rem;`;
+        
+        const frmButton = `
+        width: 100%;
+        padding: 1rem 0rem;`;
+        
+        const formButton = `
+        background-color: #60A5FA;
+        border: 2px #60A5FA solid;
+        color: #fff;
+        padding: .5rem 1rem;
+        font-size: 1.25rem;
+        line-height: 1.5;
+        border-radius: .3rem;
+        text-decoration: none;`;
+
+        const mailBody=`
+        <table style="${pageWrapper}">
+            <tr style="${tblRow}">
+                <td>
+                    <table style="${frmWrapper}">
+                        <tr style="${tblRow}">
+                            <td style="${frmLogo}">
+                            <img style="${frmLogoImg}" src="cid:confirmationPagelogo"/>
+                            </td>
+                        </tr>
+                        <tr style="${tblRow}">
+                            <td style="${frmTitle}">Welcome to Automated Immigration System</td>
+                        </tr>
+                        <tr style="${tblRow}">
+                            <td style="${frmText}">Please confirm your email by clicking on following link</td>
+                        </tr>
+                        <tr style="${tblRow}">
+                            <td style="${frmButton}">
+                                <a style="${formButton}" href="${link}" target="_blank">${buttonText}</button>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>`;
+
+
+        //Mailing System
+        await nodemailer.createTestAccount();
+        const transporter = nodemailer.createTransport(config.mail);
+
+        
+        const info = await transporter.sendMail({
+            from: `Do Not Reply ${config.mail.auth.user}`,
+            to: userInfo.email,
+            subject: mailSubject,
+            html:mailBody,
+            attachments: [
+    
+                {
+                  filename: 'aiRect.png',
+                  path: path.join(__dirname,'../','../','src','public','images','aiRect.png'),
+                  cid: 'confirmationPagelogo',
+    
+                },
+    
+            ]
+        });
+
+        return({
+            'type': 'success',
+            'data': `Message Sent to ${info.to}`,
+        });
+
+    }catch(e){
+        return({
+            'type': 'error',
+            'data':e.message
+        });
+    }
+
+};
 
 helpers.replaceIllegalChars= async function(str){
     var result = str;
