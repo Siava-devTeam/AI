cc4cd64cd12cc81f040b5965a6632d9c0bf7351f
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2021-04-05 14:13:51 -0700

    modifed default country to CA, dafault phone format replaces digits with x

--- src/public/js/intlTelInput.js
+++ src/public/js/intlTelInput.js
@@ -80,7 +80,12 @@
             // modify the parentClass
             customContainer: "",
             // modify the auto placeholder
-            customPlaceholder: null,
+            customPlaceholder: function(selectedCountryPlaceholder, selectedCountryData) {
+                const regexpCoordinates = /\d/g;
+                return(selectedCountryPlaceholder.replace(regexpCoordinates,'x'));
+                
+                // return "e.g. " + selectedCountryPlaceholder;
+              },
             // append menu to specified element
             dropdownContainer: null,
             // don't display these countries
@@ -92,7 +97,7 @@
             // inject a hidden input with this name, and on submit, populate it with the result of getNumber
             hiddenInput: "",
             // initial country
-            initialCountry: "",
+            initialCountry: "ca",
             // localized country names e.g. { 'de': 'Deutschland' }
             localizedCountries: null,
             // don't insert international dial codes
@@ -102,7 +107,7 @@
             // number type to use for placeholders
             placeholderNumberType: "MOBILE",
             // the countries at the top of the list. defaults to united states and united kingdom
-            preferredCountries: [ "us", "gb" ],
+            preferredCountries: [ "US", "CA", "IR" ],
             // display the country dial code next to the selected flag so it's not part of the typed number
             separateDialCode: false,
             // specify the path to the libphonenumber script to enable validation/formatting
